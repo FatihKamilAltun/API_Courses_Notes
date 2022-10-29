5901c5734fa35a8e079c152072b684717b117b29
FatihKamilAltun <fkaltun70@gmail.com>
2022-10-29 23:02:05 +0300

    29/10/2022-5

---
+++ src/test/java/get_request/Get08.java
@@ -0,0 +1,72 @@
+package get_request;
+
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get08 extends JsonplaceholderBaseUrl {
+
+
+    // De-Serialization : Json datayı java objesine cevirme
+    // Serialization    : Java objesini Json formatına cevirme
+    /*
+    Given
+       https://jsonplaceholder.typicode.com/todos/2
+   When
+       I send GET Request to the URL
+   Then
+       Status code is 200
+       And "completed" is false
+       And "userId" is 1
+       And "title" is "quis ut nam facilis et officia qui"
+       And header "Via" is "1.1 vegur"
+       And header "Server" is "cloudflare"
+       {
+           "userId": 1,
+           "id": 2,
+           "title": "quis ut nam facilis et officia qui",
+           "completed": false
+       }
+    */
+
+    @Test
+    public void get01() {
+
+        // Set the Url
+        spec.pathParams("first", "todos", "second", 2);
+
+        // set the expected data ==> payload
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId", 1);
+        expectedData.put("id", 2);
+        expectedData.put("title", "quis ut nam facilis et officia qui");
+        expectedData.put("completed", false);
+        System.out.println("Expected data : " + expectedData);
+
+        System.out.println(expectedData);
+
+        // send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        // do assertion
+        Map<String, Object> actualData = response.as(HashMap.class); // de-serialization
+        System.out.println("Actual data : " + actualData);
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("id"),actualData.get("id"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals("1.1 vegur",response.header("Via"));
+        assertEquals("cloudflare",response.header("Server"));
+        assertEquals(200,response.statusCode());
+
+
+    }
+}
