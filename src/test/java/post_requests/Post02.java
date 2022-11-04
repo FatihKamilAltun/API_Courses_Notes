62365f3241cacb24294c147945455affe2b1c08b
FatihKamilAltun <fkaltun70@gmail.com>
2022-11-04 20:58:08 +0300

    4/11/2022-3

---
+++ src/test/java/post_requests/Post02.java
@@ -0,0 +1,49 @@
+package post_requests;
+
+import base_url.RestfulBaseUrl;
+import org.junit.Test;
+
+public class Post02 extends RestfulBaseUrl {
+     /*
+   Given
+       1) https://restful-booker.herokuapp.com/booking
+       2) {
+            "firstname": "John",
+            "lastname": "Doe",
+            "totalprice": 11111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2021-09-09",
+                "checkout": "2021-09-21"
+             }
+          }
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 200
+       And response body should be like {
+                                           "bookingid": 5315,
+                                           "booking": {
+                                               "firstname": "John",
+                                               "lastname": "Doe",
+                                               "totalprice": 11111,
+                                               "depositpaid": true,
+                                               "bookingdates": {
+                                                   "checkin": "2021-09-09",
+                                                   "checkout": "2021-09-21"
+                                               }
+                                           }
+                                        }
+*/
+
+    @Test
+    public void post01() {
+        // Set the url
+        spec.pathParam("first","booking");
+
+        // Set the expected data
+
+
+
+    }
+}
